0ebb79b7570195cebd3b293ca913153f80b3b8ae
PranavPadmanabhan <pranavpadmanabhan77@gmail.com>
2022-09-22 19:42:02 +0530

    subscription plan tab added in profile

---
+++ src/pages/profile/subscriptionPlan/SubScriptionPlanCard.js
@@ -0,0 +1,107 @@
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import React from "react";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { IconButton } from "@mui/material";
+import PlanContentItem from "./PlanContentItem";
+
+// eslint-disable-next-line react/prop-types
+function SubScriptionPlanCard({ isSubscribed }) {
+  const [isExpanded, setIsExpanded] = React.useState(false);
+  return (
+    <MDBox
+      width="18vw"
+      minHeight="65vh"
+      bgColor="transparent"
+      margin="10px"
+      borderRadius="md"
+      shadow="sm"
+      sx={{
+        position: "relative",
+      }}
+    >
+      <IconButton
+        sx={{
+          position: "absolute",
+          top: -12,
+          right: -12,
+        }}
+        size="small"
+        color="inherit"
+      >
+        <CheckCircleIcon
+          sx={{ color: isSubscribed ? "green" : "gray" }}
+          fontSize="medium"
+        />
+      </IconButton>
+      <MDBox
+        width="100%"
+        height="50%"
+        bgColor="transparent"
+        display="flex"
+        flexDirection="column"
+      >
+        <MDTypography fontSize="smaller" alignSelf="flex-start" margin="10px">
+          Upgrade
+        </MDTypography>
+      </MDBox>
+      <MDBox
+        width="100%"
+        minHeight="50%"
+        bgColor="#25428a"
+        borderRadius="md"
+        display="flex"
+        flexDirection="column"
+        alignItems="flex-start"
+        justifyContent="flex-start"
+        paddingTop="3%"
+        paddingLeft="5%"
+      >
+        <PlanContentItem content="Free Brand Promotion" />
+        <PlanContentItem content="Business revenue" />
+        <PlanContentItem content="Reach more customers" />
+        <PlanContentItem content="Customers Engagement" />
+        <PlanContentItem content="Create Your Brand Identity" />
+        <PlanContentItem content="Brand Stoeies" />
+        <MDBox
+          width="40%"
+          height="25px"
+          borderRadius="xl"
+          bgColor="white"
+          color="#25428a"
+          cursor="pointer"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          marginBottom="5px"
+          fontSize="0.9rem"
+          sx={{
+            cursor: "pointer",
+          }}
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          Know more
+        </MDBox>
+        {isExpanded && (
+          <MDBox
+            minHeight="10vh"
+            width="100%"
+            color="white"
+            fontSize=".5rem"
+            textAlign="justify"
+            paddingRight="3%"
+            paddingBottom="2%"
+          >
+            loremNisi id velit non ea irure.Esse non culpa aliquip eu ut eiusmod
+            aute duis fugiat commodo est. Ipsum pariatur deserunt exercitation
+            minim sunt sunt culpa deserunt Lorem irure dolor adipisicing
+            officia. Nulla anim ullamco labore nisi velit in sunt voluptate quis
+            in.
+          </MDBox>
+        )}
+      </MDBox>
+    </MDBox>
+  );
+}
+
+export default SubScriptionPlanCard;
